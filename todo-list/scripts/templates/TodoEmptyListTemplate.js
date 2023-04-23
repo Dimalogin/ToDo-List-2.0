b962d23a524fa1ec68402cf8d56cbee0ed770c18
DimaLoginovskiy <remen.2000@mail.ru>
2023-04-23 19:39:32 +0300

    feat: add templates for todo-list

---
+++ todo-list/scripts/templates/TodoEmptyListTemplate.js
@@ -0,0 +1,11 @@
+export const todoEmptyListTemplate = `
+<li class="list-task task-empty">
+<img
+  class="task-empty__icon"
+  src="./images/box-empty.png"
+  alt="empty"
+  width="55"
+/>
+<div class="task-empty__title">List Empty</div>
+</li>
+`;
